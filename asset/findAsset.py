ecc5bb89f17ff100a688fea56d49fd9f71576158
highkim0922 <shineest96@gmail.com>
2022-08-24 14:23:50 +0900

    add pipeline

--- asset/findAsset.py
+++ asset/findAsset.py
@@ -2,15 +2,30 @@ import boto3
 import pymysql
 import json
 
-ENDPOINT="mysql.c14b7b28namw.ap-northeast-2.rds.amazonaws.com" # rds endpoint
-PORT="3306"
-USER="admin"
-REGION="ap-northeast-2"
-DBNAME="uplus"
+ENDPOINT = "mysql.c14b7b28namw.ap-northeast-2.rds.amazonaws.com"  # rds endpoint
+PORT = "3306"
+USER = "rdsproxyadmin"
+REGION = "ap-northeast-2"
+DBNAME = "uplus"
 
-def lambda_handler(event,context):
+def get_secret():
+    client = boto3.client('secretsmanager')
+
+    response = client.get_secret_value(
+        SecretId='mysqlDatabaseSecret'
+    )
+
+    database_secrets = json.loads(response['SecretString'])
+    print(database_secrets['password'])
+
+
+def lambda_handler(event, context):
+    print(event)
+
+    get_secret()
     client = boto3.client('rds')
     token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
+    print(token)
     try:
         conn = pymysql.connect(host=ENDPOINT, user=USER, passwd=token, database=DBNAME)
         cur = conn.cursor()

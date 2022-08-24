4ce5e4e401f608a64096c4ee96f8125ba30a4409
highkim0922 <shineest96@gmail.com>
2022-08-24 13:03:36 +0900

    add pipeline

---
+++ asset/findAsset.py
@@ -0,0 +1,23 @@
+import boto3
+import pymysql
+
+ENDPOINT="mysql.c14b7b28namw.ap-northeast-2.rds.amazonaws.com" # rds endpoint
+PORT="3306"
+USER="admin"
+REGION="ap-northeast-2"
+DBNAME="uplus"
+
+def lambda_handler(event,context):
+    client = boto3.client('rds')
+    token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
+    try:
+        conn = pymysql.connect(host=ENDPOINT, user=USER, passwd=token, database=DBNAME)
+        cur = conn.cursor()
+        cur.execute("""SELECT now()""")
+        query_results = cur.fetchall()
+        print(query_results)
+    except Exception as e:
+        print("Database connection failed due to {}".format(e))
+
+
+

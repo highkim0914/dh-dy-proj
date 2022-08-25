928c0dbf900f4084a08fd778c93d868c58579102
dyparkkk <dkdlel1z@naver.com>
2022-08-25 14:16:38 +0900

    ing

--- metadata/findMetadata.py
+++ metadata/findMetadata.py
@@ -1,4 +1,32 @@
 import json
+from mysqlConnect import getCursor
 
 def lambda_handler(event, context):
-    return json.dumps(event);
+    # if event['queryStringParameters']:
+
+    if 'page' in event['queryStringParameters']: page = event['queryStringParameters']['page']
+    # size = event['queryStringParameters']['page']
+    # sort = event['queryStringParameters']['sort']
+    primary = event['queryStringParameters']['primary']
+    secondary = event['queryStringParameters']['secondary']
+    # asset = event['queryStringParameters']['asset']
+    # project_id = event['queryStringParameters']['project_id']
+    # bar = event['queryStringParameters']['bar']
+
+    # print(event['queryStringParameters']['page'])
+
+    cursor = getCursor()
+
+    sql = "select * from metadata" + \
+    primary if !primary else None
+
+    cursor.execute("")
+    rows = cursor.fetchall()
+    cursor.connection.close()
+    return {
+        "statusCode": 200,
+        "body": json.dumps(rows)
+    }
+
+
+def dynamicQuery(condition):
--- metadata/findPrimaryCategory.py
+++ metadata/findPrimaryCategory.py
@@ -1,4 +1,17 @@
 import json
+from mysqlConnect import getCursor
 
 def lambda_handler(event, context):
-    return json.dumps(event);
+    cursor = getCursor()
+    cursor.execute("select * from primary_category")
+    rows = cursor.fetchall()
+    # print(rows)
+
+    res = [dict((cursor.description[i][0], value) \
+          for i, value in enumerate(row)) for row in rows]
+
+    cursor.connection.close()
+    return {
+        "statusCode": 200,
+        "body": json.dumps(res, ensure_ascii=False)
+    }

a6dc5abdcced8f9fd51a43b937f902299111d3a4
highkim0922 <shineest96@gmail.com>
2022-08-25 09:04:27 +0900

    modify : pymysql 연결로 변경

--- asset/findAsset.py
+++ asset/findAsset.py
@@ -4,9 +4,10 @@ import json
 
 ENDPOINT = "mysql.c14b7b28namw.ap-northeast-2.rds.amazonaws.com"  # rds endpoint
 PORT = "3306"
-USER = "rdsproxyadmin"
+USER = "admin"
 REGION = "ap-northeast-2"
 DBNAME = "uplus"
+
 
 def get_secret():
     client = boto3.client('secretsmanager')
@@ -16,30 +17,25 @@ def get_secret():
     )
 
     database_secrets = json.loads(response['SecretString'])
-    print(database_secrets['password'])
-
+    return database_secrets['password']
 
 def lambda_handler(event, context):
-    print(event)
-
-    get_secret()
-    client = boto3.client('rds')
-    token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
-    print(token)
     try:
-        conn = pymysql.connect(host=ENDPOINT, user=USER, passwd=token, database=DBNAME)
+        conn = pymysql.connect(host=ENDPOINT, user=USER, passwd=get_secret(), database=DBNAME)
         cur = conn.cursor()
         cur.execute("SELECT * FROM asset")
         query_results = cur.fetchall()
         print(query_results)
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "hello world"
+            }),
+        }
     except Exception as e:
         print("Database connection failed due to {}".format(e))
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world"
-        }),
-    }
+
 
 
 

16c6c13e12301eae300f7a287e6884b9a0041a27
highkim0922 <shineest96@gmail.com>
2022-08-25 12:37:34 +0900

    modify : pathVariable 처리하는 query로 변경

--- asset/findAsset.py
+++ asset/findAsset.py
@@ -1,3 +1,4 @@
+import datetime
 import boto3
 import pymysql
 import json
@@ -7,7 +8,6 @@ PORT = "3306"
 USER = "admin"
 REGION = "ap-northeast-2"
 DBNAME = "uplus"
-
 
 def get_secret():
     client = boto3.client('secretsmanager')
@@ -19,23 +19,25 @@ def get_secret():
     database_secrets = json.loads(response['SecretString'])
     return database_secrets['password']
 
+def get_str_value(obj):
+    if isinstance(obj, datetime.datetime):
+        return str(obj)
+    else:
+        return obj
 def lambda_handler(event, context):
+    asset_id = event['pathParameters']['asset_id']
+    print(asset_id)
     try:
         conn = pymysql.connect(host=ENDPOINT, user=USER, passwd=get_secret(), database=DBNAME)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM asset")
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        cur.execute(f'SELECT * FROM asset inner join asset_image_urls as aiu on asset.id = aiu.asset_id where asset.id = {asset_id}')
         query_results = cur.fetchall()
-        print(query_results)
+        for i in range(len(query_results)):
+            query_results[i] = {obj:get_str_value(query_results[i][obj]) for obj in query_results[i].keys()}
         return {
-            "isBase64Encoded": False,
+            #"isBase64Encoded": False,
             "statusCode": 200,
-            "body": json.dumps({
-                "message": "hello world"
-            }),
+            "body": json.dumps(query_results)
         }
     except Exception as e:
         print("Database connection failed due to {}".format(e))
-
-
-
-

f2561013aa331b1d769cfe295b3e9c8bd17f426a
dyparkkk <dkdlel1z@naver.com>
2022-08-26 11:18:19 +0900

    Add addMetadata, updateMetadata, deleteMetadata at metadata

--- metadata/deleteMetadata.py
+++ metadata/deleteMetadata.py
@@ -1,4 +1,25 @@
 import json
+from mysqlConnect import getDictCursor
 
 def lambda_handler(event, context):
-    return json.dumps(event);
+
+    # 1. 파싱
+    id = event['pathParameters']['metadata_id']
+
+    cursor = getDictCursor()
+    # 2. keyword_metadata delete
+    sql = f'delete from keyword_metadata where metadata_id = {id}'
+    print(sql)
+    cursor.execute(sql)
+
+    # 3. metadata delete
+    sql = f'delete from metadata where id = {id}'
+    print(sql)
+    cursor.execute(sql)
+
+    # 4. 커밋
+    # cursor.connection.commit()
+    cursor.connection.close()
+    return {
+        "statusCode": 204
+    }

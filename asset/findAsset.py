0d9b3ee8e80b34d1b887ebea078ae61bea1b77a9
highkim0922 <shineest96@gmail.com>
2022-08-26 10:15:58 +0900

    modify : get_str_value 추가

--- asset/findAsset.py
+++ asset/findAsset.py
@@ -4,20 +4,29 @@ import pymysql
 import json
 from mysqlConnect import *
 
+
+def get_str_value(obj):
+    if isinstance(obj, datetime.datetime):
+        return str(obj)
+    else:
+        return obj
+
+
 def lambda_handler(event, context):
     asset_id = event['pathParameters']['asset_id']
     print(asset_id)
     try:
         conn = get_connection()
         cur = get_dict_cursor(conn)
-        cur.execute(f'SELECT * FROM asset inner join asset_image_urls as aiu on asset.id = aiu.asset_id where asset.id = {asset_id}')
+        cur.execute(
+            f'SELECT * FROM asset inner join asset_image_urls as aiu on asset.id = aiu.asset_id '
+            f'where asset.id = {asset_id}')
         query_results = cur.fetchall()
         for i in range(len(query_results)):
-            query_results[i] = {obj:get_str_value(query_results[i][obj]) for obj in query_results[i].keys()}
+            query_results[i] = {obj: get_str_value(query_results[i][obj]) for obj in query_results[i].keys()}
         return {
             "statusCode": 200,
             "body": json.dumps(query_results)
         }
     except Exception as e:
         print("Database connection failed due to {}".format(e))
-

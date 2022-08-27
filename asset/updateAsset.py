dda35bed44e6fe49bddb5290a9887ef5790cf411
highkim0922 <shineest96@gmail.com>
2022-08-27 23:59:55 +0900

    modify : 쿼리 생성 로직 수정

--- asset/updateAsset.py
+++ asset/updateAsset.py
@@ -4,36 +4,44 @@ import pymysql
 import json
 from dbConnect import *
 
+
 def lambda_handler(event, context):
     body_json = json.loads(event['body'])
-    id = body_json['id']
+    asset_id = body_json['id']
     name = body_json['name']
     asset_url = body_json['asset_url']
     image_urls = body_json['image_urls']
     details = body_json['details']
     now = str(datetime.datetime.now())
-    request_username = event['requestContext']['authorizer']['claims']['cognito:username']
+    username = event['requestContext']['authorizer']['claims']['cognito:username']
 
     try:
         conn = get_connection()
         cur = get_dict_cursor(conn)
-        select_query = f'SELECT * FROM asset where id = {id}'
+        select_query = f'SELECT * FROM asset where id = {asset_id}'
         print(select_query)
         cur.execute(select_query)
         # 기존 에셋 정보 - 이후 로그 작성 시 사용할 예정
         select_result = cur.fetchone()
-        update_query = f"UPDATE asset SET `name` = '{name}', `asset_url` = '{asset_url}', `updater` = '{request_username}, " \
-                       f"`details` = '{details}', `updated_at` = '{now}' "
+        update_query = f"UPDATE asset SET `name` = '{name}'" \
+                       f", `updater` = '{username}" \
+                       f", `details` = '{details}'" \
+                       f", `updated_at` = '{now}'"
+        if asset_url != "https://cognito-test-dy.s3.ap-northeast-2.amazonaws.com/dh/null":
+            update_query = update_query + f", `asset_url` = {asset_url}"
+        update_query = update_query + f" where id = {asset_id} "
         print(update_query)
         cur.execute(update_query)
 
-        delete_image_urls_query = f"DELETE FROM asset_image_urls where asset_id = {id}"
-        cur.execute(delete_image_urls_query)
-        for url in image_urls:
-            image_url_value = (id, url)
-            insert_image_url_query = f'INSERT INTO asset_image_urls (`asset_id`, `url`) VALUES {image_url_value}'
-            print(insert_image_url_query)
-            cur.execute(insert_image_url_query)
+        if image_urls != "null":
+            delete_image_urls_query = f"DELETE FROM asset_image_urls where asset_id = {asset_id}"
+            cur.execute(delete_image_urls_query)
+            for url in image_urls:
+                image_url_value = (asset_id, url)
+                insert_image_url_query = f'INSERT INTO asset_image_urls (`asset_id`, `url`) VALUES {image_url_value}'
+                print(insert_image_url_query)
+                cur.execute(insert_image_url_query)
+
         conn.commit()
         return {
             "statusCode": 200,

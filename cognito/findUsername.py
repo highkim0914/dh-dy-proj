c0d4a626d3b28058a8c3ad85fbc69da42fb23279
dyparkkk <dkdlel1z@naver.com>
2022-08-30 08:35:53 +0900

    Modify cognito:group -> cognito:grops at findUsername

--- cognito/findUsername.py
+++ cognito/findUsername.py
@@ -9,10 +9,9 @@ def lambda_handler(event, context):
     request_username = event['requestContext']['authorizer']['claims']['cognito:username']
     request_group = ''
     try:
-        request_group = event['requestContext']['authorizer']['claims']['cognito:group']
+        request_group = event['requestContext']['authorizer']['claims']['cognito:groups']
     except Exception as e:
         request_group = ''
-
     return {
         "statusCode": 200,
         "body": json.dumps({
